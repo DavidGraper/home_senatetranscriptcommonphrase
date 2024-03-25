c39511a7b4d65cdf36f34d0c73e909e32ccf7716
David Graper <dgraper@fastmail.com>
2024-03-25 16:35:53 -0400

    Working version with about 200 regex expressions

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 import re
-
 import SenateSQLDB
+import fitz
+from io import BytesIO
+
+
+def highlight_pdffile(pdffilelines):
+
+    for info in pdffilelines:
+        page2update = int(info["pdfpage"])
+        line2update = int(info["pdfline"]) - 1
+        text2update = info["pdftext"]
+
+        # Search for text
+        page = pdfDoc[page2update]
+
+        # Get page lines
+        page_lines = page.get_text("text").split('\n')
+
+        # Get rectangles for each line
+        line_rects = []
+
+        # Substring search
+        needle = page_lines[line2update]
+
+        cliprect = page.search_for(needle)
+
+        # the_rect = page.search_for(text2update)
+        the_rect = page.search_for(text2update, clip=cliprect[0])
+
+        # highlight = page.add_squiggly_annot(the_rect)
+        highlight = page.add_highlight_annot(the_rect)
+
+        highlight.update()
+    # out = open("output.txt", "wb")  # create a text output
+    # for page in doc:  # iterate the document pages
+    #     text = page.get_text().encode("utf8")  # get plain text (is in UTF-8)
+    #     out.write(text)  # write text of page
+    #     out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
+    # out.close()
+    # Save to output
+    pdfDoc.save(output_buffer)
+    pdfDoc.close()
+    # Save the output buffer to the output file
+    with open("2023-05-02_highlighted.pdf", mode='wb') as f:
+        f.write(output_buffer.getbuffer())
 
 
 def loadsenateregexes():
@@ -15,6 +58,11 @@ def loadsenateregexes():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    pdfDoc = fitz.open("2023-05-02.pdf")  # open a document
+
+    # Save the generated PDF to memory buffer
+    output_buffer = BytesIO()
 
     # Get list of senate common phrase regexes
     senateregexes = loadsenateregexes()
@@ -49,8 +97,11 @@ if __name__ == '__main__':
                     pdflinestohighlight.append(pdfresult)
 
                 print("Match: {0} : {1}".format(transcriptline, senateregex))
+                print("")
                 matchlinecount += 1
                 break
+
+    highlight_pdffile(pdflinestohighlight)
 
     print("Total lines: {0}".format(str(transcriptlinecount)))
     print("Matching lines: {0}".format(str(matchlinecount)))
